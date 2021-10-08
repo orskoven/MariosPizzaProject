04b00ba154d6815687b015677e4b34a751bdddfb
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-08 15:56:43 +0200

    pizza class

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,13 @@
 public class Pizza {
+    private String name;
+    private int pizzaNumber;
+    private Ingrediens[] ingrediens;
+
+    public Pizza(String name, int pizzaNumber, Ingrediens[] ingrediens) {
+        this.name = name;
+        this.pizzaNumber = pizzaNumber;
+        this.ingrediens = ingrediens;
+    }
 }
+
+
