4466359766d72dfea8914eaaf02e22903fb5185d
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-11 10:34:30 +0200

    #3

--- src/Order.java
+++ src/Order.java
@@ -34,4 +34,6 @@ public abstract class Order extends Pizza{
                 ", orderName='" + orderName + '\'' +
                 '}';
     }
+
+
 }
--- src/PhoneOrder.java
+++ src/PhoneOrder.java
@@ -1,2 +1,5 @@
-public class PhoneOrder {
+public class PhoneOrder extends Order{
+    public PhoneOrder(int pizzaNumber, String name, Ingrediens[] ingrediens, int price, Pizza[] pizzas, int orderId, String orderName) {
+        super(pizzaNumber, name, ingrediens, price, pizzas, orderId, orderName);
+    }
 }
